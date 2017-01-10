e79d169e1c0edc8efe44696172ad737e0fc7aa87
Keith Holliday <krh121791@gmail.com>
2017-01-09 19:59:54 -0700

    Fixed twitter and facebook auth

--- config/passport.js
+++ config/passport.js
@@ -66,133 +66,97 @@ module.exports = function(passport) {
         });
   }));
 
-    // passport.use(new FacebookStrategy({
-    //
-    //     // pull in our app id and secret from our auth.js file
-    //     clientID        : configAuth.facebookAuth.clientID,
-    //     clientSecret    : configAuth.facebookAuth.clientSecret,
-    //     callbackURL     : configAuth.facebookAuth.callbackURL,
-    //     passReqToCallback : true
-    //
-    // },
-    //
-    // // facebook will send back the token and profile
-    // function(req, token, refreshToken, profile, done) {
-    //
-    //     // asynchronous
-    //     process.nextTick(function() {
-    //       if (!req.user) {
-    //         // find the user in the database based on their facebook id
-    //         User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-    //
-    //             // if there is an error, stop everything and return that
-    //             // ie an error connecting to the database
-    //             if (err)
-    //                 return done(err);
-    //
-    //             // if the user is found, then log them in
-    //             if (user) {
-    //                 return done(null, user); // user found, return that user
-    //             } else {
-    //                 // if there is no user found with that facebook id, create them
-    //                 var newUser            = new User();
-    //
-    //                 // set all of the facebook information in our user model
-    //                 newUser.facebook.id    = profile.id; // set the users facebook id
-    //                 newUser.facebook.token = token; // we will save the token that facebook provides to the user
-    //                 newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-    //                 newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-    //
-    //                 // save our user to the database
-    //                 newUser.save(function(err) {
-    //                     if (err)
-    //                         throw err;
-    //
-    //                     // if successful, return the new user
-    //                     return done(null, newUser);
-    //                 });
-    //             }
-    //
-    //         });
-    //       } else {
-    //         // user already exists and is logged in, we have to link accounts
-    //         var user            = req.user; // pull the user out of the session
-    //
-    //         // update the current users facebook credentials
-    //         user.facebook.id    = profile.id;
-    //         user.facebook.token = token;
-    //         user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-    //         user.facebook.email = profile.emails[0].value;
-    //
-    //         // save the user
-    //         user.save(function(err) {
-    //             if (err)
-    //                 throw err;
-    //             return done(null, user);
-    //         });
-    //       }
-    //     });
-    //
-    // }));
+  passport.use(new FacebookStrategy({
+    clientID        : nconf.get('facebookAuth:clientID'),
+    clientSecret    : nconf.get('facebookAuth:clientSecret'),
+    callbackURL     : nconf.get('facebookAuth:callbackURL'),
+    passReqToCallback : true,
+  },
+  function(req, token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      if (req.user) {
+        var user = req.user;
+        user.facebook.id    = profile.id;
+        user.facebook.token = token;
+        user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+        user.facebook.email = profile.emails[0].value;
 
-    // passport.use(new TwitterStrategy({
-    //   consumerKey     : configAuth.twitterAuth.consumerKey,
-    //   consumerSecret  : configAuth.twitterAuth.consumerSecret,
-    //   callbackURL     : configAuth.twitterAuth.callbackURL,
-    //   passReqToCallback : true
-    // },
-    // function(req, token, tokenSecret, profile, done) {
-    //     // make the code asynchronous
-    //   // User.findOne won't fire until we have all our data back from Twitter
-    //   process.nextTick(function() {
-    //     if (!req.user) {
-    //       User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-    //
-    //           // if there is an error, stop everything and return that
-    //           // ie an error connecting to the database
-    //           if (err)
-    //               return done(err);
-    //
-    //           // if the user is found then log them in
-    //           if (user) {
-    //               return done(null, user); // user found, return that user
-    //           } else {
-    //               // if there is no user, create them
-    //               var newUser                 = new User();
-    //
-    //               // set all of the user data that we need
-    //               newUser.twitter.id          = profile.id;
-    //               newUser.twitter.token       = token;
-    //               newUser.twitter.username    = profile.username;
-    //               newUser.twitter.displayName = profile.displayName;
-    //
-    //               // save our user into the database
-    //               newUser.save(function(err) {
-    //                   if (err)
-    //                       throw err;
-    //                   return done(null, newUser);
-    //               });
-    //           }
-    //       });
-    //     } else {
-    //       var user                 = req.user;
-    //
-    //       // set all of the user data that we need
-    //       user.twitter.id          = profile.id;
-    //       user.twitter.token       = token;
-    //       user.twitter.username    = profile.username;
-    //       user.twitter.displayName = profile.displayName;
-    //
-    //       // save our user into the database
-    //       user.save(function(err) {
-    //           if (err)
-    //               throw err;
-    //           return done(null, user);
-    //       });
-    //     }
-    //   });
-    // }));
-    //
+        return user.save()
+          .then(function() {
+            return done(null, user);
+          })
+          .catch(function (err) {
+            throw err;
+          });
+      }
+
+      return User.findOne({ 'facebook.id' : profile.id })
+        .then(function(user) {
+            var newUser          = new User();
+
+            newUser.facebook.id    = profile.id; // set the users facebook id
+            newUser.facebook.token = token; // we will save the token that facebook provides to the user
+            newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+            // newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+            return newUser.save();
+        })
+        .then(function(newUser) {
+          if (newUser) return done(null, newUser);
+          return done(null, user);
+        })
+        .catch(function (err) {
+            return done(err);
+        });
+    });
+  }));
+
+  passport.use(new TwitterStrategy({
+    consumerKey     : nconf.get('twitterAuth:consumerKey'),
+    consumerSecret    : nconf.get('twitterAuth:consumerSecret'),
+    callbackURL     : nconf.get('twitterAuth:callbackURL'),
+    passReqToCallback : true
+  },
+  function(req, token, tokenSecret, profile, done) {
+    process.nextTick(function() {
+      if (req.user) {
+        var user          = req.user;
+
+        user.twitter.id          = profile.id;
+        user.twitter.token       = token;
+        user.twitter.username    = profile.username;
+        user.twitter.displayName = profile.displayName;
+
+        return user.save()
+          .then(function() {
+            return done(null, user);
+          })
+          .catch(function (err) {
+            throw err;
+          });
+      }
+
+      return User.findOne({ 'twitter.id' : profile.id })
+        .then(function(user) {
+            var newUser          = new User();
+
+            newUser.twitter.id          = profile.id;
+            newUser.twitter.token       = token;
+            newUser.twitter.username    = profile.username;
+            newUser.twitter.displayName = profile.displayName;
+
+            return newUser.save();
+        })
+        .then(function(newUser) {
+          if (newUser) return done(null, newUser);
+          return done(null, user);
+        })
+        .catch(function (err) {
+            return done(err);
+        });
+    });
+  }));
+
 
     passport.use(new GoogleStrategy({
       clientID        : nconf.get('googleAuth:clientID'),
