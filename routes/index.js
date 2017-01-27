88ac2a5ec21496945ba783e26b57688c1c891ad2
Keith Holliday <krh121791@gmail.com>
2017-01-26 18:03:11 -0700

    Removed some unused routes

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,6 @@ var jwt    = require('jsonwebtoken');
 var Donations = require('../models/donations');
 var User = require('../models/user');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kibbl' });
 });
@@ -22,21 +21,9 @@ router.get('/login', function(req, res) {
 
 router.post('/login-angular', passport.authenticate('local-login'), function(req, res) { res.send(req.user); });
 
-router.post('/login', passport.authenticate('local-login', {
-  successRedirect : '/profile', // redirect to the secure profile section
-  failureRedirect : '/login', // redirect back to the signup page if there is an error
-  failureFlash : true // allow flash messages
-}));
-
 router.get('/register', function(req, res) {
   res.render('register.jade', { message: req.flash('signupMessage') });
 });
-
-router.post('/register', passport.authenticate('local-signup', {
-  successRedirect : '/profile', // redirect to the secure profile section
-  failureRedirect : '/register', // redirect back to the signup page if there is an error
-  failureFlash : true // allow flash messages
-}));
 
 router.post('/api/v1/register', function (req, res) {
   let email = req.body.email;
@@ -103,60 +90,39 @@ router.post('/api/v1/login', function (req, res) {
     });
 });
 
-router.get('/profile', Middleware.isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
-        user : req.user // get the user out of session and pass to template
-    });
-});
-
-// =====================================
-// FACEBOOK ROUTES =====================
-// =====================================
-// route for facebook authentication and login
-router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
-
-// handle the callback after facebook has authenticated the user
-router.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-        successRedirect : '/profile',
-        failureRedirect : '/'
-    }));
-
-// =====================================
-// TWITTER ROUTES ======================
-// =====================================
-// route for twitter authentication and login
-router.get('/auth/twitter', passport.authenticate('twitter'));
-
-// handle the callback after twitter has authenticated the user
-router.get('/auth/twitter/callback',
-    passport.authenticate('twitter', {
-        successRedirect : '/profile',
-        failureRedirect : '/'
-    }));
-
 router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
 
-// =====================================
-// GOOGLE ROUTES =======================
-// =====================================
-// send to google to do the authentication
-// profile gets us their basic information including their name
-// email gets their emails
+router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+      successRedirect : '/',
+      failureRedirect : '/'
+  })
+);
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+  passport.authenticate('twitter', {
+      successRedirect : '/',
+      failureRedirect : '/'
+  })
+);
+
 router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-// the callback after google has authenticated the user
 router.get('/auth/google/callback',
-        passport.authenticate('google', {
-                successRedirect : '/profile',
-                failureRedirect : '/'
-        }));
+  passport.authenticate('google', {
+          successRedirect : '/',
+          failureRedirect : '/'
+  })
+);
 
 router.post('/charge', Middleware.isLoggedIn, function(req, res, next) {
-  console.log(req.body)
   stripe.customers.create({
     email: req.user.local.email,
     source: req.body.stripeToken,
@@ -168,7 +134,6 @@ router.post('/charge', Middleware.isLoggedIn, function(req, res, next) {
     });
   }).then(function(charge) {
     // Donations
-    console.log(charge)
     //@TODO: Log the donation to model
     //@TODO: Send email
     res.render('index', { message: 'Your donation as been sent!', status: 'Success!' });
