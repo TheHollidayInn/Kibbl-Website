eb962d40e5dc70eb10d7362778b3671a3f7bd07b
Keith Holliday <krh121791@gmail.com>
2017-03-05 11:04:35 -0700

    Add how section

--- public/javascripts/controllers/HomeCtrl.js
+++ public/javascripts/controllers/HomeCtrl.js
@@ -1,8 +1,44 @@
 angular.module('Kibbl')
 	.controller('HomeCtrl', ['$scope', 
 		function ($scope) {
-      console.log("home")
+      
 			$scope.events = [
+				{
+					name: 'Test 1',
+				},
+				{
+					name: 'Test 2',
+				},
+				{
+					name: 'Test 3',
+				},
+			];
+
+			$scope.pets = [
+				{
+					name: 'Test 1',
+				},
+				{
+					name: 'Test 2',
+				},
+				{
+					name: 'Test 3',
+				},
+			];
+
+			$scope.opportunities = [
+				{
+					name: 'Test 1',
+				},
+				{
+					name: 'Test 2',
+				},
+				{
+					name: 'Test 3',
+				},
+			];
+
+			$scope.shelters = [
 				{
 					name: 'Test 1',
 				},
