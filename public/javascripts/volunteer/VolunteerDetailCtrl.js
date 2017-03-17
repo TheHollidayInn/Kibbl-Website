91b6c6936a6cc107d3e920f25c06da9aeab4ece2
Keith Holliday <krh121791@gmail.com>
2017-03-16 22:27:12 -0600

    Fixed volunteer detail

--- public/javascripts/volunteer/VolunteerDetailCtrl.js
+++ public/javascripts/volunteer/VolunteerDetailCtrl.js
@@ -1,7 +1,18 @@
 angular.module('Volunteer')
-	.controller('VolunteerDetailCtrl', ['$scope', 
-		function ($scope) {
-			$scope.opportunity = {
-        name: 'TEsting',
-      };
-		}]);
+.controller('VolunteerDetailCtrl', ['$scope', '$routeParams', '$http',
+		function ($scope, $routeParams, $http) {
+		$scope.volunteer = {};
+
+		sendRequest();
+		function sendRequest() {
+			var url = '/api/v1/volunteer/' + $routeParams.id;
+
+			$http({
+				method: 'GET',
+				url: url,
+			})
+			.then(function (response) {
+				$scope.volunteer = response.data.data;
+			})
+		}
+	}]);
