7d876bed166fa49d3f1537aa39fb105f66bab0ec
Keith Holliday <krh121791@gmail.com>
2017-03-16 22:14:29 -0600

    Updated item details on home

--- public/javascripts/events/EventDetailCtrl.js
+++ public/javascripts/events/EventDetailCtrl.js
@@ -1,7 +1,18 @@
 angular.module('Events')
-	.controller('EventDetailCtrl', ['$scope', 
-		function ($scope) {
-			$scope.event = {
-        name: 'TEsting',
-      };
+	.controller('EventDetailCtrl', ['$scope', '$routeParams', '$http',
+		function ($scope, $routeParams, $http) {
+			$scope.event = {};
+
+			sendRequest();
+			function sendRequest() {
+				var url = '/api/v1/events/' + $routeParams.id;
+
+				$http({
+					method: 'GET',
+					url: url,
+				})
+				.then(function (response) {
+					$scope.event = response.data.data;
+				})
+			}
 		}]);
