c82f45897e7539c0a9dc9f79c27144dee8a8f51f
Keith Holliday <krh121791@gmail.com>
2017-06-28 19:00:52 -0600

    ADded compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression')
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -31,6 +32,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression);
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
