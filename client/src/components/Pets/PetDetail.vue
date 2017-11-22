74ce4d66c8eb013c7c5da05ecdb1c8e6c5a273ba
thehollidayinn <krh121791@gmail.com>
2017-11-21 22:13:51 -0600

    Added pet detail info

--- client/src/components/Pets/PetDetail.vue
+++ client/src/components/Pets/PetDetail.vue
@@ -11,16 +11,16 @@ div
             div.img-feature(:style="`background-image:url(${pet.media[0].urlSecureFullsize})`")
         .col-md-3.action-buttons
           a.btn.btn-primary(v-if='pet.contact && pet.contact.url', :href="pet.contact.url", target="_blank") Webpage
-          button.btn.btn-raised.btn-primary.btn-favorite(ng-click='favorite()', v-if='!pet.favorited')
+          button.btn.btn-raised.btn-primary.btn-favorite(@click='favorite()', v-if='!pet.favorited')
             | Favorite
-          button.btn.btn-raised.btn-primary.btn-favorite-active(ng-click='favorite()', v-if='pet.favorited')
+          button.btn.btn-raised.btn-primary.btn-favorite-active(@click='favorite()', v-if='pet.favorited')
             | Remove
   .container(style="margin-top: 2rem;")
     .row
       .col-md-8
         .well
           h3 Description
-          //p(style="white-space: pre-wrap;") {{decodeHTMLEntities(pet.description)}}
+          p(style="white-space: pre-wrap;") {{description}}
         comments(:item-id='pet._id', v-if='pet._id')
       .col-md-3
         .well
@@ -51,6 +51,7 @@ div
 <script>
   import axios from 'axios'
   import Comments from '@/components/Comments'
+  import { parameterize } from '@/libs/queryHelper'
 
   export default {
     name: 'PetDetail',
@@ -59,9 +60,7 @@ div
     },
     data () {
       return {
-        pet: {},
-        facebookUrl: '',
-        twitterUrl: ''
+        pet: {}
       }
     },
     mounted () {
@@ -71,6 +70,62 @@ div
         .then((response) => {
           this.pet = response.data.data
         })
+    },
+    computed: {
+      twitterUrl () {
+        const url = window.location.href
+        const tweetDetails = {
+          url,
+          text: 'Check out this amazing pet on Kibbl: '
+        }
+        return 'https://twitter.com/intent/tweet?' + parameterize(tweetDetails)
+      },
+      facebookUrl () {
+        const url = window.location.href
+        return `https://www.facebook.com/sharer/sharer.php?u=${url}&src=sdkpreparse`
+      },
+      description () {
+        let text = this.pet.description
+        if (!text) return ''
+
+        const entities = [
+          ['amp', '&'],
+          ['apos', '\''],
+          ['#x27', '\''],
+          ['#x2F', '/'],
+          ['#39', '\''],
+          ['#47', '/'],
+          ['lt', '<'],
+          ['gt', '>'],
+          ['nbsp', ' '],
+          ['quot', '"'],
+          ['ldquo', '"'],
+          ['rsquo', '"'],
+          ['#039', '\'']
+        ]
+
+        for (let i = 0, max = entities.length; i < max; ++i) {
+          text = text.replace(new RegExp('&' + entities[i][0] + ';', 'g'), entities[i][1])
+        }
+
+        let tweetShit = `Tweet!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');  (function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();`
+        let tweetShitIndex = text.indexOf(tweetShit)
+        if (tweetShitIndex !== -1) {
+          text = text.replace(tweetShit, '')
+        }
+
+        return text
+      }
+    },
+    methods: {
+      favorite () {
+        this.pet.favorited = !this.pet.favorited
+
+        axios.post('/api/v1/favorites', {
+          type: 'pet',
+          itemId: this.pet._id
+        })
+      }
     }
   }
 </script>
